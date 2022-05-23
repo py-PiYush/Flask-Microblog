f686a594edd23014baeccb8d0ebab560a31969f9
Piyush <impiyush016@gmail.com>
2022-05-23 13:01:25 +0530

    Restructured Project

--- app/__init__.py
+++ app/__init__.py
@@ -1,22 +1,41 @@
 import logging
 import os
 from logging.handlers import SMTPHandler, RotatingFileHandler
-from flask import Flask
+from flask import Flask, request
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_babel import Babel
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
-login.login_view = "login"
+login.login_view = "auth.login"
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
+babel = Babel(app)
+
+
+from app.auth import bp as auth_bp
+
+app.register_blueprint(auth_bp, url_prefix="/auth")
+
+from app.main import bp as main_bp
+
+app.register_blueprint(main_bp)
+
+
+from app.errors import bp as errors_bp
+
+app.register_blueprint(errors_bp)
 
 if not app.debug:
     if app.config["MAIL_SERVER"]:
@@ -53,4 +72,10 @@ if not app.debug:
     app.logger.setLevel(logging.INFO)
     app.logger.info("Microblog startup")
 
-from app import routes, models, errors
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+from app import models
