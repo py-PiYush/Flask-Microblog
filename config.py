087b7fc8c96974495632dd57dcc730d5f07a9303
Piyush <impiyush016@gmail.com>
2022-05-31 12:14:34 +0530

    Deploy

--- config.py
+++ config.py
@@ -7,8 +7,8 @@ load_dotenv(os.path.join(basedir, ".env"))
 
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "DATABASE_URL"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "").replace(
+        "postgres://", "postgresql://"
     ) or "sqlite:///" + os.path.join(basedir, "app.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get("MAIL_SERVER")
@@ -19,3 +19,4 @@ class Config(object):
     ADMINS = ["your-email@example.com"]
     POSTS_PER_PAGE = 10
     LANGUAGES = ["en", "es"]
+    LOG_TO_STDOUT = os.environ.get("LOG_TO_STDOUT")
